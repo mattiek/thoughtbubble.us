b72af49bbdcf583dd5bc75e25140bfc7c1e633d1
blorenz <blorenz@gmail.com>
2013-08-28 23:15:16 -0400

    Adding photo preview to signup

--- thoughtbubble/static/js/tb/tb-signup.js
+++ thoughtbubble/static/js/tb/tb-signup.js
@@ -3,3 +3,20 @@
 $('#id_captcha_choicefield img').click(function(e) {
     $('#id_captcha').val($(e.target).attr('data-choice'));
 })
+
+//$('#add-photo').fileInput();
+var options = {
+    resize: 10 * 1024, //resize images using a canvas element until they're under 10KB
+    quality: 0.6, //JPEG compression, defaults to 0.8
+    decode: true //use window.atob to base64 decode before checking size
+};
+
+$('#add-photo').imagePreviewInput({ resize: 30 * 1024, decode: true, quality: 0.5 }).on('load', function(e, data) {
+    var $preview = $('#add-photo').css('display', 'inline-block'),
+        $img = $preview.find('img');
+    if (!$img.length) {
+        $img = $('<img/>').appendTo($preview);
+    }
+
+    $img.attr('src', data);
+});
