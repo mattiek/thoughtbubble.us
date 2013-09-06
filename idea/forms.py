5515906f0ff4da35b97a6fbb18762474e8570a30
blorenz <blorenz@gmail.com>
2013-09-05 20:38:50 -0400

    Filter to only Columbus and order neighborhoods by name

--- idea/forms.py
+++ idea/forms.py
@@ -11,7 +11,7 @@ class AddIdeaForm(forms.Form):
     name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'title'}),)
     description = forms.CharField(widget=forms.Textarea(),)
     what_kind = forms.ModelChoiceField(queryset=IdeaType.objects.all(), empty_label='')
-    where = forms.ModelChoiceField(queryset=Neighborhood.objects.all(), empty_label='')
+    where = forms.ModelChoiceField(queryset=Neighborhood.objects.filter(city='Columbus').order_by('name'), empty_label='')
     what_for = forms.ChoiceField(choices=FOR_CHOICES_AND_EMPTY,widget=forms.Select())
 
     pic1 = forms.ImageField(widget=FilePicker, required=False)
