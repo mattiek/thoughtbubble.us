39895c26258d5dddce9b0b2ae4932d7071238948
blorenz <blorenz@gmail.com>
2013-09-30 23:13:36 -0400

    Idea page

--- idea/urls.py
+++ idea/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('idea.views',
    url(r'^(?P<state>\w+)/?$', IdeaList.as_view(), name='idea_list'),
    url(r'^(?P<state>\w+)/(?P<city>\w+)/?$', IdeaList.as_view(), name='idea_list'),
    url(r'^(?P<state>\w+)/(?P<city>\w+)/(?P<community>[-\w]+)/?$', IdeaList.as_view(), name='idea_list'),
-   url(r'^(?P<state>\w+)/(?P<city>\w+)/(?P<community>[-\w]+)/(?P<pk>\d+)/?$', IdeaDetail.as_view(), name='idea_detail'),
+   url(r'^(?P<state>\w+)/(?P<city>\w+)/(?P<community>[-%\w ]+)/(?P<pk>\d+)/?$', IdeaDetail.as_view(), name='idea_detail'),
 )
