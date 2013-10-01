8bc2f34abcae3e3933643daaabd4db054935ff41
blorenz <blorenz@gmail.com>
2013-10-01 10:13:49 -0400

    Styling of ideas and dashboards

--- thoughtbubble/templatetags/ideacountables.py
+++ thoughtbubble/templatetags/ideacountables.py
@@ -1,5 +1,6 @@
 from django import template
 from idea.models import Idea, IdeaSupport
+from community.models import Community
 
 
 register = template.Library()
@@ -7,6 +8,11 @@ register = template.Library()
 @register.filter(name='idea_count')
 def idea_count(value):
     return Idea.objects.filter(user=value).count()
-#
-# def get_idea_support_count(self):
-#     return IdeaSupport.objects.filter(user=self.user).count()
+
+@register.filter(name='idea_support_count')
+def get_idea_support_count(value):
+    return IdeaSupport.objects.filter(user=value).count()
+
+@register.filter(name='communities_joined_count')
+def get_idea_support_count(value):
+    return Community.objects.filter(members=value).count()
