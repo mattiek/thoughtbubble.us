181432be24c9f900a7a3c1bcd685b1b24fb84e8b
blorenz <blorenz@gmail.com>
2013-10-13 11:11:01 -0400

    Getting URLs location specific

---
+++ thoughtbubble/geo_urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/?$', 'thoughtbubble.views.home', name='home'),
+    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/ideas/', include('idea.urls')),
+    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/locations/', include('location.urls')),
+    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/communities/', include('community.urls')),
+    url(r'^(?P<state>\w+)/(?P<city>\w+)/explore/?$', 'thoughtbubble.views.explore', name='explore'),
+    url(r'^(?P<state>\w+)/(?P<city>\w+)/explore/(?P<pk>\d+)/?$', 'thoughtbubble.views.explore', name='explore'),
+    )
--- thoughtbubble/urls.py
+++ thoughtbubble/urls.py
@@ -8,32 +8,33 @@ from views import MyConnectionsView, UserProfileFormView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'thoughtbubble.views.home', name='home'),
-    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/?$', 'thoughtbubble.views.home', name='home'),
-    url(r'^signup/?$', 'thoughtbubble.views.signup', name='signup'),
 
-    # url(r'^accounts/profile/?$', 'thoughtbubble.views.dashboard', name='user_dashboard'),
-    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/ideas/', include('idea.urls')),
-    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/locations/', include('location.urls')),
-    url(r'^(?P<state>\w+)/(?P<city>[-%\w ]+)/communities/', include('community.urls')),
-
-    url(r'^supporting/', include('supportering.urls')),
-    url(r'^login/?$', 'thoughtbubble.views.login', name='login'),
-    url(r'^logout/?$', 'thoughtbubble.views.logout', name='logout'),
-    url(r'^explore/?$', 'thoughtbubble.views.explore', name='explore'),
-    url(r'^(?P<state>\w+)/(?P<city>\w+)/explore/?$', 'thoughtbubble.views.explore', name='explore'),
-    url(r'^(?P<state>\w+)/(?P<city>\w+)/explore/(?P<pk>\d+)/?$', 'thoughtbubble.views.explore', name='explore'),
-    url(r'^privacy-policy/?$', 'thoughtbubble.views.privacy', name='privacy'),
-    url(r'^comments/', include('django.contrib.comments.urls')),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include('api.urls')),
-    # url('', include('social.apps.django_app.urls', namespace='social')),
     (r'^accounts/', include('allauth.urls')),
     (r'^avatar/', include('avatar.urls')),
     url(r'^accounts/profile/?$', MyConnectionsView.as_view(), name='user_dashboard'),
     url(r'^accounts/update/?$', UserProfileFormView.as_view(), name='user_profile_update'),
+    url(r'^comments/', include('django.contrib.comments.urls')),
+
+    url(r'^$', 'thoughtbubble.views.home', name='home'),
+
+    url(r'^signup/?$', 'thoughtbubble.views.signup', name='signup'),
+
+    # url(r'^accounts/profile/?$', 'thoughtbubble.views.dashboard', name='user_dashboard'),
+
+    url(r'^supporting/', include('supportering.urls')),
+
+    url(r'^login/?$', 'thoughtbubble.views.login', name='login'),
+    url(r'^logout/?$', 'thoughtbubble.views.logout', name='logout'),
+    url(r'^explore/?$', 'thoughtbubble.views.explore', name='explore'),
+
+    url(r'^privacy-policy/?$', 'thoughtbubble.views.privacy', name='privacy'),
+
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('api.urls')),
+    url(r'^geo/', include('thoughtbubble.geo_urls')),
+    # url('', include('social.apps.django_app.urls', namespace='social')),
+
 
 
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
