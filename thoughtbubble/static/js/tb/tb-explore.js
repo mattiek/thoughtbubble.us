6613be4673713023720841ebbc2113224916ce9f
brandon <blorenz@gmail.com>
2014-01-25 14:58:20 -0500

    Removing debuggers

--- thoughtbubble/static/js/tb/tb-explore.js
+++ thoughtbubble/static/js/tb/tb-explore.js
@@ -210,7 +210,6 @@ var getNeighborhoods = function() {
                 if (feature.properties.orgs) {
                     $('#minisplore-wrapper ul').html('');
                     _.each(feature.properties.orgs, function(e) {
-                        debugger
                     $('#minisplore-wrapper ul').append('<li><a href="' + e.properties.explore +'">' + e.properties.title + '<a/></li>');
                     });
                     $('#minisplore').fadeIn();
