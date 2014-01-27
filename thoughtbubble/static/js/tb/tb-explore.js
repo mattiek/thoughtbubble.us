c3f887f97004877bdc87fa8f88954c25a21ab9f7
brandon <blorenz@gmail.com>
2014-01-27 00:07:52 -0500

    Trying fastclick for explore

--- thoughtbubble/static/js/tb/tb-explore.js
+++ thoughtbubble/static/js/tb/tb-explore.js
@@ -107,30 +107,6 @@ var experimentalMarkers = function(d) {
 }
 
 TB.Map.mapLayer.on('ready', function() {
-//    $.ajax({
-//        url:  '/api/v1/organizations/.json',
-//        dataType: 'json',
-//        success: function load(d) {
-//
-//            // Transform the regions to the centers
-//            var dStuff = _.map(d.features, function(obj) {
-//                return {
-//                    id: obj.id,
-//                    properties: obj.properties,
-//                    type: obj.type,
-//                    geometry: obj.center
-//                }
-//            });
-//
-//           var latestD = {
-//                type: "FeatureCollection",
-//                features: dStuff
-//            }
-//            experimentalMarkers(latestD);
-//            getNeighborhoods();
-//
-//        }
-//    });
     getNeighborhoods();
     TB.Map.map().on('moveend', function(e) {
         getNeighborhoods();
@@ -204,7 +180,7 @@ var getNeighborhoods = function() {
             // Listen for individual marker clicks
             TB.Map.map().markerLayer.on('touchstart click',function(e) {
 //                e.layer.unbindPopup();
-                e.stopPropogation();
+                e.stopPropagation();
                 e.preventDefault();
 
                 if(e.handled !== true) {
