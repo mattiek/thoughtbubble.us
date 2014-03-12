55b64cc254224fb13a642afcd9acee90981f720b
brandon <blorenz@gmail.com>
2014-03-12 19:43:10 -0400

    Admin fixes

--- geo/location/urls.py
+++ geo/location/urls.py
@@ -4,8 +4,8 @@ from views import LocationList, LocationCreate, LocationDetail, LocationUpdate
 urlpatterns = patterns('geo.location.views',
    # url(r'^add/?$', 'addlocation', name='addlocation'),
    # url(r'^add/?$', LocationCreate.as_view(), name='addlocation'),
-   url(r'^add/(?P<organization>[\w-]+)/?$', LocationCreate.as_view(), name='addlocation'),
-   url(r'^list/?$', LocationList.as_view(), name='location_list'),
+   url(r'^add/(?P<place>[\w-]+)/(?P<organization>[\w-]+)/?$', LocationCreate.as_view(), name='addlocation'),
+   url(r'^list/(?P<place>[\w-]+)/(?P<organization>[\w-]+)/?$', LocationList.as_view(), name='location_list'),
    url(r'^detail/(?P<place>[\w-]+)/(?P<organization>[\w-]+)/(?P<location>[\w-]+)/?$', LocationDetail.as_view(), name='location_detail'),
    url(r'^edit/(?P<place>[\w-]+)/(?P<organization>[\w-]+)/(?P<location>[\w-]+)/?$', LocationUpdate.as_view(), name='location_update'),
 )
