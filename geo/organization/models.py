c637ed204db2fe5bf1d098819e6433423d183d27
brandon <blorenz@gmail.com>
2014-04-01 12:33:56 -0400

    getting facebook sharing in

--- geo/organization/models.py
+++ geo/organization/models.py
@@ -218,7 +218,7 @@ class Organization(models.Model):
     def get_fb_sharing(self):
         return {
             'share_copy': 'Check out this org!',
-            'share_image': urllib.quote_plus('http://thoughtbubble.us/static/images/thoughtbubble-illustration.jpg'),
+            'share_image': 'http://thoughtbubble.us/static/images/thoughtbubble-illustration.jpg',
             'share_caption': 'thoughtbubble.us',
             'share_name': 'Sharing',
             'share_link': 'http://thoughtbubble.us' + self.get_absolute_url(),
