26e4fb26cd7094b5f64bf684cba221509422d763
brandon <blorenz@gmail.com>
2014-04-01 13:34:45 -0400

    image viewer

--- thoughtbubble/static/js/tb/tb-global.js
+++ thoughtbubble/static/js/tb/tb-global.js
@@ -140,3 +140,18 @@ $('.share-fb').on('click', function(e){
         picture: fbDataShareImage
     });
 });
+
+$('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+        verticalFit: true
+    },
+    zoom: {
+        enabled: true,
+        duration: 300 // don't foget to change the duration also in CSS
+    }
+});
