51af6fd7bd1d25e004a4b25e1734d89c25659e05
brandon <blorenz@gmail.com>
2014-04-14 18:56:17 -0400

    Partner flubbing

--- partner/models.py
+++ partner/models.py
@@ -5,3 +5,8 @@ class Partner(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     img = models.ImageField(upload_to=path_and_rename('partner','img'))
     link = models.URLField(blank=True, null=True)
+
+    def __unicode__(self):
+        if self.name:
+            return self.name
+        return 'Partner Object'
