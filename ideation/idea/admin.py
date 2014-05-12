21bb251d9ba5c637d1d927c8fb83aee5f762a1f8
brandon <blorenz@gmail.com>
2014-05-12 10:13:40 -0400

    Filtering on idea

--- ideation/idea/admin.py
+++ ideation/idea/admin.py
@@ -7,7 +7,11 @@ class IdeaTypeAdmin(admin.ModelAdmin):
     list_display = ('name','ordering',)
     list_editable = ('ordering',)
 
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_filter = ['status',]
+
 admin.site.register(IdeaType, IdeaTypeAdmin)
 admin.site.register(IdeaImage)
 admin.site.register(IdeaLink)
-admin.site.register(Idea)
+admin.site.register(Idea, IdeaAdmin)
